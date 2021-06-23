22b06917bd8577bd947e411f43e99a31486a3870
Joe Stringer <joe.stringer@infinityworks.com>
2021-06-23 15:43:11 +0100

    wip

---
+++ terraform/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "aws_caller_identity" {
+  value = data.aws_caller_identity.current
+}
